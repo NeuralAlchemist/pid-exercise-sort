ead23925330dc7b25d1c7c9edf953167cd9a9d86
Yash Seeta <yashseeta94@gmail.com>
2021-03-11 03:59:53 +0100

    Updated selection sort to swap only after the inner loop has finished

--- src/main/java/pid/exercise/sort/SelectionSort.java
+++ src/main/java/pid/exercise/sort/SelectionSort.java
@@ -4,13 +4,18 @@ public class SelectionSort {
 
     public static void sort(int[] array) {
         int min;
+        int minIdx;
+        //Add a minIndex to keep track of the minimum, and swap only at the last element
         for(int i = 0; i <= array.length-2; i++){
             min = array[i];
+            minIdx = i;
             for(int j = i+1; j <= array.length-1; j++){
-                if(min > array[j]){
-                    swapElements(array, i, j);
-                    min = array[i];
+                if(array[minIdx] > array[j]){
+                    minIdx = j;
                 }
+            }
+            if(i != minIdx){
+                swapElements(array, i, minIdx);
             }
         }
     }
